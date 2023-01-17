fc4e6ef662eb349e4f00d9d8dc0c10f5fd2bcf32
KuzStudyGit <helmeik@yandex.com>
2023-01-17 15:52:39 +0300

    add base methods for Todo

--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,28 @@
-class Todo {}
+class Todo {
+    constructor({ todos = [], inputSelector, formSelector, listSelector }) {
+        this.todos = todos;
+        this.input = document.querySelector(inputSelector);
+        this.form = document.querySelector(formSelector);
+        this.list = document.querySelector(listSelector);
+    }
+
+    setTodos(todos) {
+        this.todos = todos;
+    }
+
+    addTodo(todo) {
+        this.setTodos([todo, ...this.todos]);
+    }
+
+    deleteTodo(id) {
+        this.setTodos(this.todos.filter((todo) => todo.id !== id));
+    }
+
+    toggleTodo(id) {
+        this.setTodos(
+            this.todos.map((todo) =>
+                todo.id === id ? { ...todo, checked: !todo.checked } : todo
+            )
+        );
+    }
+}
